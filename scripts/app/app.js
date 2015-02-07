56b516d4245359a7465ad90d0e664a07fae1b369
Conor Hayes <ch1987@live.ie>
2015-02-07 17:16:40 +0000

    Added Home view. Added CSS file for view. Added RouteProvider function for navigation between views.

--- scripts/app/app.js
+++ scripts/app/app.js
@@ -5,7 +5,17 @@
     'ui.bootstrap',
 ]);
 
-app.controller('MainCtrl', ['$scope', '$location', '$routeParams', 'FireBaseService', '$rootScope', function ($scope, $location, $routeParams, FireBaseService, $rootScope) {
+app.config(function ($routeProvider) {
+    $routeProvider
+    .when('/home',
+    {
+        templateUrl: '/views/home.html',
+        controller: 'MainCtrl'
+    })
+    .otherwise({ redirectTo: '/home' });
+});
+
+app.controller('MainCtrl', ['$scope', '$location', '$routeParams', function ($scope, $location, $routeParams) {
     $scope.isActive = function (viewLocation) {
         return $location.path().indexOf(viewLocation) >= 0;
     };
