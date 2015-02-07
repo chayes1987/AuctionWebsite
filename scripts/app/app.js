40d3fb1a38748f978e3e32485fbdaf9616202b39
Conor Hayes <ch1987@live.ie>
2015-02-07 16:30:03 +0000

    Added required CSS and JS libraries. Added Index.html with angular integration. Added navbar to page.

---
+++ scripts/app/app.js
@@ -0,0 +1,12 @@
+﻿var app = angular.module('AuctionApp', [
+    'ngRoute',
+    'ngAnimate',
+    'firebase',
+    'ui.bootstrap',
+]);
+
+app.controller('MainCtrl', ['$scope', '$location', '$routeParams', 'FireBaseService', '$rootScope', function ($scope, $location, $routeParams, FireBaseService, $rootScope) {
+    $scope.isActive = function (viewLocation) {
+        return $location.path().indexOf(viewLocation) >= 0;
+    };
+}]);
